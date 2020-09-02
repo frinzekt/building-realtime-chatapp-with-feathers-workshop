75f9c6e2cfaac00f143a0dfffec8244c7593f552
frinzekt <frinzekt@gmail.com>
2020-09-02 11:33:55 +0800

    exposing the REST API and partially exposing sockets over Express Server

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const socketio = require('@feathersjs/socketio');
 // CREATING AN EXPRESS SERVER
 // EXPRESS IS A LIBRARY THAT HELPS IN CREATING SERVER APPLICATIONS IN THE NODEJS PLATFORM
 // NODEJS IS JUST PLATFORM/RUNTIME TO RUN JAVASCRIPT OUTSIDE OF THE BROWSER
-const app = feathers();
+const app = express(feathers());
 
 // FEATHERS SERVICES AND METHOD/FUNCTIONS FOR THE SERVICES
 class MessageService {
@@ -29,25 +29,17 @@ class MessageService {
 	}
 }
 
+// middlewares
+app.use(express.static(__dirname)); //SERVING OF FRONTEND FILES
+app.configure(express.rest()); // ENABLING RESTFUL ACCESS OF FEATHERS
+app.configure(socketio()); // EXPOSING FEATHERS OVER SOCKET
+
 // ATTACHMENT OF A SERVICE TO THE FEATHERS APPLICATION - SERVICES RESTFUL ROUTE "/"
 // SERVICE ROUTE WILL HAVE "/", but can be referenced as normal "messages" without "/"
 app.use('/messages', new MessageService());
 
-const main = async () => {
-	await app.service('messages').create({ text: 'Hello Feathers' });
-	await app.service('messages').create({ text: 'Hello again' });
-	const messages = await app.service('messages').find();
-	console.log('All messages', messages);
-};
-
-main();
-
-// LET'S JUST COMMENT THIS FOR NOW, SO WE CAN DEMONSTRATE FEATHERS ON ITS OWN
-// //MIDDLEWARE ARE SERVICES THAT HAPPENS IN THE MIDDLE OF REQUEST AND EXECUTION OF MAIN TASKS
-// app.use(express.static(__dirname)); // SERVING OF STATIC/FRONTEND FILES
-
-// // PREVENTS THE SCRIPT FROM EXITTING BY LETTING IT LISTEN ON A SPECIFIC PORT
-// // RUNNING A NODE APPLICATION REQUIRES TYPING THE COMMAND "node app.js" LIKE PYTHON "python main.py"
-// app.listen(3030).on('listening', () => {
-// 	console.log('Feathers server running on localhost:3030');
-// })
+// PREVENTS THE SCRIPT FROM EXITTING BY LETTING IT LISTEN ON A SPECIFIC PORT
+// RUNNING A NODE APPLICATION REQUIRES TYPING THE COMMAND "node app.js" LIKE PYTHON "python main.py"
+app.listen(3030).on('listening', () => {
+	console.log('Feathers server running on localhost:3030');
+});
